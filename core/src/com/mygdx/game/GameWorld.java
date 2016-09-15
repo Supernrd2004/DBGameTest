70547958aef574e1cc4ca22a2bd43405bce8200c
Supernrd2004 <supernerd2004@gmail.com>
2016-09-14 21:37:17 -0700

    Added ortho camera

--- core/src/com/mygdx/game/GameWorld.java
+++ core/src/com/mygdx/game/GameWorld.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.utils.TimeUtils;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class GameWorld extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -16,6 +17,10 @@ public class GameWorld extends ApplicationAdapter {
 	Texture img2;
 
 	private boolean loggingEnabled = true;
+
+	private OrthographicCamera camera;
+	int width = 1280;
+	int height = 1024;
 
 	private double timeStart;
 	private double timeEnd;
@@ -29,6 +34,7 @@ public class GameWorld extends ApplicationAdapter {
 	public void create () {
 
 		SetupFPSCounter();
+		SetupCamera();
 
 		fpsLog = new FPSLogger();
 
@@ -51,6 +57,10 @@ public class GameWorld extends ApplicationAdapter {
 
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.draw(img2, 400, 400);
@@ -77,6 +87,12 @@ public class GameWorld extends ApplicationAdapter {
 		font.setColor(Color.GREEN);
 		font.getData().setScale(4.0f);
 		WriteToLog("FPS Counter Initialized");
+	}
+
+	private void SetupCamera(){
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, width, height);
 	}
 
 	private void WriteToLog(String message){
