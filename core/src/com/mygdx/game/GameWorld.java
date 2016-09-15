015cefe714ef40e99f9507ab2d001323c8a5aa8f
theonlybrien <brien.developer@gmail.com>
2016-09-14 21:20:49 -0700

    add FPS to screen

--- core/src/com/mygdx/game/GameWorld.java
+++ core/src/com/mygdx/game/GameWorld.java
@@ -3,6 +3,7 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -12,6 +13,7 @@ import com.badlogic.gdx.utils.TimeUtils;
 public class GameWorld extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Texture img2;
 
 	private boolean loggingEnabled = true;
 
@@ -20,14 +22,19 @@ public class GameWorld extends ApplicationAdapter {
 	private java.lang.String fpsString;
 	private double fpsCounter;
 	private BitmapFont font;
+	private FPSLogger fpsLog;
+	private int framesPerSecondLabel;
 
 	@Override
 	public void create () {
 
 		SetupFPSCounter();
 
+		fpsLog = new FPSLogger();
+
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		img2 = new Texture("Jellyfish.jpg");
 		WriteToLog("Create Method Finished");
 	}
 
@@ -37,11 +44,17 @@ public class GameWorld extends ApplicationAdapter {
 		timeStart = TimeUtils.millis() / 1000.0;
 		fpsString = "FPS" + fpsCounter;
 
+		fpsLog.log();
+		framesPerSecondLabel = Gdx.graphics.getFramesPerSecond();
+
+		Integer.toString(framesPerSecondLabel);
+
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
-		font.draw(batch,fpsString,10.0f, 10.0f);
+		batch.draw(img2, 400, 400);
+		font.draw(batch,Integer.toString(framesPerSecondLabel),300.0f, 300.0f);
 		batch.end();
 	}
 
@@ -51,6 +64,7 @@ public class GameWorld extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		img2.dispose();
 	}
 
 	private void SetupFPSCounter(){
